45b3e1fd7bb82d921fb9714beb3aa15653a629d8
James Clancey <james.clancey@gmail.com>
2021-01-13 14:06:25 -0900

    Most tests are now up and working!

--- src/Forms/test/Xamarin.Forms.Core.UnitTests/SolidColorBrushTests.cs
+++ src/Forms/test/Xamarin.Forms.Core.UnitTests/SolidColorBrushTests.cs
@@ -15,7 +15,7 @@ namespace Xamarin.Forms.Core.UnitTests
 		public void TestConstructor()
 		{
 			SolidColorBrush solidColorBrush = new SolidColorBrush();
-			Assert.AreEqual("[Color: A=-1, R=-1, G=-1, B=-1, Hue=-1, Saturation=-1, Luminosity=-1]", solidColorBrush.Color.ToString(), "Color");
+			Assert.IsNull(solidColorBrush.Color,"Color");
 		}
 
 		[Test]
